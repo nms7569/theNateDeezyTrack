b7adeccd0326e23673a722d3499c33a59356b613
Nathanael Shaw <nms7569@rit.edu>
2017-03-30 17:02:14 -0400

    added prob 6 with timer printout

--- src/pe_problem6.cpp
+++ src/pe_problem6.cpp
@@ -2,4 +2,20 @@
 
 void pe_problem6(void){
     std::cout << "Problem #6" << std::endl;
+    unsigned long int finalValue;
+    unsigned long int sumOfSquares = 0;
+    unsigned long int squareSum = 0;
+    clock_t my_time;
+    my_time = clock();
+
+    for (int i; i <= 100; ++i){
+	    sumOfSquares += i*i;
+	    squareSum += i;
+    }
+    squareSum = squareSum * squareSum;
+    finalValue = squareSum - sumOfSquares;
+
+    my_time = clock() - my_time;
+    std::cout << "value: " << finalValue << std::endl;
+    std::cout << "time: " << ((float)my_time)/CLOCKS_PER_SEC << std::endl;
 };
