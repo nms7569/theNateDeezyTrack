9d5a7e5dfc727ba3ed25fe59b2451dca75d90f47
Nathanael Shaw <nms7569@rit.edu>
2017-03-30 21:41:46 -0400

    intermediate push

--- src/pe_problem22.cpp
+++ src/pe_problem22.cpp
@@ -2,4 +2,25 @@
 
 void pe_problem22(void){
     std::cout << "Problem #22" << std::endl;
+    clock_t myTime;
+    myTime = clock();
+    std::ofstream fileOut;
+    std::ifstream fileIn;
+    string p = "~/Downloads/p022_names.txt";
+    string line;
+
+    if (boost::filesystem::exists(p)){
+	    if (boost::filesystem::is_regular_file(p)){
+		    fileIn.open(p);
+		    if (fileIn.is_open()){
+			    while(std::getline(fileIn,line)){
+
+			    }
+			    fileIn.close();
+		    }
+	    }
+    }
+    myTime = clock()-myTime;
+
+    std::cout << "time: " << ((float)myTime)/CLOCKS_PER_SEC << std::endl;
 };
--- src/pe_problem22.h
+++ src/pe_problem22.h
@@ -2,8 +2,10 @@
 #define PE_PROBLEM22_H_INCLUDED
 
 #include <iostream>
+#include <fstream>
 #include <ctime>
 #include <vector>
+#include <boost\filesystem.hpp>
 
 void pe_problem22(void);
 
