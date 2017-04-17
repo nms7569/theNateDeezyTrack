af0f179ceddad3078a785b3302ead7ec8a283493
Nathanael Shaw <nms7569@rit.edu>
2017-04-16 21:46:10 -0400

    more leftovers

---
+++ src/problem.cpp
@@ -0,0 +1,5 @@
+#include "problem.h"
+
+void evaluate(void){
+	std::cout << "Problem class cannot be evaluated, instantiate a child." << std::endl;
+}
---
+++ src/problem.h
@@ -0,0 +1,6 @@
+#include <iostream>
+
+class Problem{
+	protected:
+		void evaluate(void);
+}
