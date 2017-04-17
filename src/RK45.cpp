eb4c41923d5e60566a5f85ce74eb45bf9bd1c35e
Nathanael Shaw <nms7569@rit.edu>
2017-04-17 00:24:08 -0400

    update to eom class method

--- src/RK45.cpp
+++ src/RK45.cpp
@@ -11,12 +11,16 @@
  * Outputs:
  *          - void
  */
-RK45::RK45(callback_function new_zdot, double new_t0, double new_tf, double new_h, boost::numeric::ublas::vector<double> new_z0){
- 	this->zdot = new_zdot;
+//RK45::RK45(const callback_function& new_zdot, const double& new_t0, const double& new_tf, const double& new_h, const boost::numeric::ublas::vector<double>& new_z0){
+ 	//this->zdot = new_zdot;
+	RK45::RK45(const EOM& new_eom, const double& new_t0, const double& new_tf, const double& new_h, const boost::numeric::ublas::vector<double>& new_z0, const bool& new_report){
+	this->eom = new_eom;
 	this->t0 = new_t0;
 	this->tf = new_tf;
 	this->h = new_h;
 	this->z0 = new_z0;
+	this->print_report = new_report;
+	//this->state_history
 }
 
 /**
@@ -29,9 +33,12 @@ RK45::RK45(callback_function new_zdot, double new_t0, double new_tf, double new_
  */
 boost::numeric::ublas::vector<double> RK45::integrate(){
 	double tn = this->t0;
-	boost::numeric::ublas::vector<double> zn = this->z0;
-	boost::numeric::ublas::vector<double> k1, k2, k3, k4;
-
+//	boost::numeric::ublas::vector<double> zn = this->z0;
+//	boost::numeric::ublas::vector<double> k1, k2, k3, k4;
+	std::vector<double> zn = this->z0;
+	std::vector<double> k1 (zn.size(),0), k2(zn.size(),0), k3(zn.size(),0), k4(zn.size(),0), kbuffer(zn.size(),0), zbuffer(zn.size(),0);
+	
+	/*
 	while(tn <= this->tf){
 		k1 = this->zdot(tn, zn);
 		k2 = this->zdot(tn + (this->h/2), zn + (this->h/2)*k1);
@@ -41,5 +48,95 @@ boost::numeric::ublas::vector<double> RK45::integrate(){
 		zn += (this->h/6)*(k1 + 2*k2 + 2*k3 + k4);
 		tn += this->h;
 	}
+	*/
+	
+	while(tn <= this->tf){
+		this->eom::evaluate(zn, kbuffer);
+		k1 = kbuffer;
+
+		for (int i = 0; i < zn.size(); ++i){
+			zbuffer[i] = zn[i] + (this->h/2)*k1[1];
+		}
+		this->eom::evaluate(zbuffer, kbuffer);
+		k2 = kbuffer;
+
+		for (int i = 0; i < zn.size(); ++i){
+			zbuffer[i] = zn[i] + (this->h/2)*k2[1];
+		}
+		this->eom::evaluate(zbuffer, kbuffer);
+		k3 = kbuffer;
+
+		for (int i = 0; i < zn.size(); ++i){
+			zbuffer[i] = zn[i] + (this->h)*k3[1];
+		}
+		this->eom::evaluate(zbuffer, kbuffer);
+		k4 = kbuffer;
+
+		for (int i = 0; i < zn.size(); ++i){
+			zbuffer[i] = zn[i] + (this->h/6.0)*(k1[i] + 2.0*k2[i] + 2.0*k3[i] +k4[i]);
+		zn = zbuffer;
+		tn += this->h;
+		}
+	}
 	return zn;
 }
+
+//void RK45::set_zdot(callback_function& new_zdot){
+	//this->zdot = new_zdot;
+//}
+//callback_function RK45::get_zdot(){
+	//return this->zdot;
+//}
+void RK45::set_eom(EOM& new_eom){
+	this->eom = new_eom;
+}
+EOM RK45::get_eom(){
+	return this->eom;
+}
+void RK45::set_t0(double& new_t0){
+	this->t0 = new_t0;
+}
+double RK45::get_t0(){
+	return this->t0;
+}
+void RK45::set_tf(double& new_tf){
+	this->tf = new_tf;
+}
+double RK45::get_tf(){
+	return this->tf;
+}
+void RK45::set_h(double& new_h){
+	this->h = new_h;
+}
+double RK45::get_h(){
+	return this->h;
+}
+void RK45::set_z0(boost::numeric::ublas::vector<double>& new_z0){
+	this->z0 = new_z0;
+}
+boost::numeric::ublas::vector<double> RK45::get_z0(){
+	return this->z0;
+}
+void RK45::set_report(bool& new_report){
+	this->print_report = new_report;
+}
+bool RK45::get_report(){
+	return this->print_report;
+}
+
+/*
+void RK45::report(void){
+	std::ofstream outfile ("results.txt",std::ofstream::binary);
+	outfile << "t0: " << this->t0 << "\ttf: " << this->tf << "\th: " << this->h << std::endl;
+	outfile << "with states: " << std::endl;
+	for (int i = 0; i < this->z0.size(); ++i){
+		outfile << "\tz(" << i << ") = " << z0[i] << std::endl;
+	}
+	outfile << "evaluates to: " << std::endl;
+	boost::numeric::ublas::vector<double> zf = this->integrate();
+	for (int i = 0; i <this->z0.size(); ++i){
+		outfile << "\tzf(" << i << ") = " << zf[i] << std::endl;
+	}
+	outfile.close();
+}
+*/
--- src/RK45.h
+++ src/RK45.h
@@ -1,6 +1,10 @@
 #ifndef RK45_INCLUDED
 #define RK45_INCLUDED
 #include <boost/numeric/ublas/vector.hpp>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include "EOM.h"
 
 /**
  * Name:    callback_function
@@ -11,7 +15,7 @@
  * Outputs:
  *          - (vector<double>) vector of derivative values of states
  */
-typedef boost::numeric::ublas::vector<double> (*callback_function)(double, boost::numeric::ublas::vector<double>);
+//typedef boost::numeric::ublas::vector<double> (*callback_function)(double, boost::numeric::ublas::vector<double>);
 
 /**
  * Name:    RK45
@@ -20,11 +24,15 @@ typedef boost::numeric::ublas::vector<double> (*callback_function)(double, boost
 class RK45{
 	public:
 		//constructors
-		RK45(const callback_function& new_zdot, const double& new_t0, const double& new_tf, const double& new_h, const boost::numeric::ublas::vector<double>& new_z0);
+		//RK45(const callback_function& new_zdot, const double& new_t0, const double& new_tf, const double& new_h, const boost::numeric::ublas::vector<double>& new_z0);
+		RK45(const EOM& new_eom, const double& new_t0, const double& new_tf, const double& new_h, const boost::numeric::ublas::vector<double>& new_z0, const bool& new_report);
+		~RK45(){};
 
 		//getter & setters
-		void setFunc(callback_function& new_zdot);
-		callback_function getFunc();
+		//void set_zdot(callback_function& new_zdot);
+		//callback_function get_zdot();
+		void set_eom(EOM& new_eom);
+		EOM get_eom();
 
 		void set_t0(double& new_t0);
 		double get_t0();
@@ -38,14 +46,20 @@ class RK45{
 		void set_z0(boost::numeric::ublas::vector<double>& new_z0);
 		boost::numeric::ublas::vector<double> get_z0();
 
+		void set_report(bool& new_report);
+		bool get_report();
+
 		//behaviors
 		boost::numeric::ublas::vector<double> integrate();
+		void report(void);
 
 	private:
-		callback_function zdot;
+		//callback_function zdot;
+		EOM eom;
 		double t0;
 		double tf;
 		double h;
 		boost::numeric::ublas::vector<double> z0;
+		bool print_report;
 }
 #endif
