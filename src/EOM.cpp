2ceebd02fbb313d64e8ba749bf75f9e5f66af029
devin <bunce2@illinois.edu>
2017-04-16 23:14:59 -0500

    added this-> for clarity in evaluate

--- src/EOM.cpp
+++ src/EOM.cpp
@@ -10,7 +10,7 @@ ThreeD_Pendulum::ThreeD_Pendulum(const double & m, const double & L){
 void ThreeD_Pendulum::evaluate(const std::vector<double> & initial_state, std::vector<double> & derivatives){
 // initial state is [theta, theta_dot, phi, phi_dot] and the derivatives function is one time derivative
 	derivatives[0] = initial_state[1];
-	derivatives[1] = initial_state[3]*initial_state[3]*std::cos(initial_state[0])*std::sin(initial_state[0]) - (g/L)*sin(initial_state[0]);
+	derivatives[1] = initial_state[3]*initial_state[3]*std::cos(initial_state[0])*std::sin(initial_state[0]) - (this->g/this->L)*sin(initial_state[0]);
 	derivatives[2] = initial_state[3];
 	derivatives[3] = -2.0*initial_state[3]*initial_state[1]*std::cos(initial_state[0])/std::sin(initial_state[0]);
 };
